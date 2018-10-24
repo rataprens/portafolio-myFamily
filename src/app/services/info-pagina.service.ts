13716de2843a1049808ded77868aca00ca99a539
Jose Mancilla <mancilla-jose@hotmail.com>
2018-10-24 00:16:51 -0300

    seccion 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {InfoPagina} from "../interfaces/info-pagina.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+    info: InfoPagina = {};
+    cargada = false;
+
+  constructor(private http:HttpClient) {
+      console.log("servicio cargado");
+      this.http.get("assets/data/data-pagina.json").subscribe((res: InfoPagina) =>{
+          this.cargada = true;
+          this.info = res;
+          console.log(res);
+        });
+
+  }
+}
